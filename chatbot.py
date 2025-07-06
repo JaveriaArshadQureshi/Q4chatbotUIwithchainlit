6f1beee8ed03cd1f75dda684f1387c508a3cf3e0
Javeriabintearshad@gmail.com <javeriabintearshad@gmail.com>
2025-07-06 23:55:54 +0500

    update

--- chatbot.py
+++ chatbot.py
@@ -5,5 +5,5 @@ async def main(message: cl.Message):
     # Our custom logic goes here...
     # Send a fake response back to the user
     await cl.Message(
-        content=f"Getting: {message.content}",
+        content=f"Received: {message.content}",
     ).send()
